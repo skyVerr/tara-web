30760d3215d755f6e130a6980a722bf34d7d2732
Verrrr <zylegaspi@gmail.com>
2019-04-29 20:16:03 +0800

    add admin home and admin menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { AdminHomeComponent } from "./pages/admin/admin-home/admin-home.component";
 
 const routes: Routes = [
   { path: 'home' , component: HomeComponent},
-  { path: 'login' , component: LoginComponent}
+  { path: 'login' , component: LoginComponent},
+  { path: 'admin', component: AdminHomeComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from "@angular/common/http";
 
+//Semantic ui
+import { SuiModule } from 'ng2-semantic-ui';
+import { AdminHomeComponent } from './pages/admin/admin-home/admin-home.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    AdminHomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule,
     FormsModule,
-    HttpClientModule
-    
+    HttpClientModule,
+    SuiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
